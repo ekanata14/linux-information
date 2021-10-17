31eb5e281405dee776b36a5bbd9ed2d6a7a6bf98
ekanata14 <76573128+ekanata14@users.noreply.github.com>
2021-10-17 20:18:11 +0800

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
+function replace(hide, show) {
+    document.getElementById(hide).style.display="none";
+    document.getElementById(show).style.display="block";
+    }
 
+    function replace2(hide, show){
+    document.getElementById(hide).style.display="none";
+    document.getElementById(show).style.display="block";
+    }
+    
